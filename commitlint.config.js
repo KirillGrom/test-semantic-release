8fa1fca0932c5020c4c08a961b019a20e1c118da
Gromov Kirill <k.gromov@corp.mail.ru>
2022-07-06 14:52:26 +0300

    feat(FPT-220): updated commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,4 +2,28 @@ module.exports = {
     extends: [
         "@commitlint/config-conventional"
     ],
+    rules: {
+        'scope-case': [2,'always',['kebab-case','upper-case']],
+        'scope-empty': [2, 'never'],
+        'jira-ticket':[2,'always'],
+    },
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^(\w+\[.*?\]: )(.+)/,
+            headerCorrespondence: ["type", "scope", "subject"],
+        },
+    },
+    plugins: [
+        {
+            rules: {
+                'jira-ticket': ({ scope }) => {
+                    const pattern = new RegExp('[A-Za-z]{2,}-\\d+');
+                    return [
+                        pattern.test(scope),
+                        `Your scope should contain task number`,
+                    ];
+                },
+            },
+        },
+    ],
 }
