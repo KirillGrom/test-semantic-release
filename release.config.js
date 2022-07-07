a7e738dff777f1bc045ed84c208d33b45dfd2aa3
Gromov Kirill <k.gromov@corp.mail.ru>
2022-07-07 10:58:55 +0300

    feat(dfgd-443): add changelog

--- release.config.js
+++ release.config.js
@@ -4,10 +4,16 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],
     [
       '@semantic-release/git',
       {
+        "assets": ["docs/CHANGELOG.md"],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
