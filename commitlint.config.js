69c6ae0b9ebe880a3a8a6153aeec1bf78acb95fc
Gromov Kirill <k.gromov@corp.mail.ru>
2022-07-08 11:27:06 +0300

    🔥 [FPT-323] dfgdfg dfg dfg dgf dg

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,17 @@
 const matchAnyEmojiWithSpaceAfter =
-    /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])\s/;
-const matchOptionalTicketNumberWithSpaceAfter = /(?:\[[A-Z]{2,}-\\d+]\s)?/;
+  /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])\s/;
+const matchOptionalTicketNumberWithSpaceAfter = /(?:\[(.*)\]\s)?/;
 const subjectThatDontStartWithBracket = /([^\[].+)/;
 
 module.exports = {
     parserPreset: {
         parserOpts: {
             headerPattern: new RegExp(
-                "^" +
-                matchAnyEmojiWithSpaceAfter.source +
-                matchOptionalTicketNumberWithSpaceAfter.source +
-                subjectThatDontStartWithBracket.source +
-                "$"
+              "^" +
+              matchAnyEmojiWithSpaceAfter.source +
+              matchOptionalTicketNumberWithSpaceAfter.source +
+              subjectThatDontStartWithBracket.source +
+              "$"
             ),
             headerCorrespondence: ["emoji", "ticket", "subject"],
         },
@@ -20,15 +20,15 @@ module.exports = {
         {
             rules: {
                 "jira-ticket": ({ ticket }) => {
-                    const pattern = new RegExp('[A-Za-z]{2,}-\\d+');
-                    console.log(ticket, pattern.test(ticket))
+                    const pattern = new RegExp('[A-Z]{2,}-\\d+');
                     return [
                         pattern.test(ticket),
-                        'Your ticket must be in format FTP-000'
+                        'Your ticket must be in format FPT-000'
                     ]
                 },
                 "header-match-team-pattern": (parsed) => {
                     const { emoji, ticket, subject } = parsed;
+                    console.log(parsed)
                     if (!emoji && !ticket && !subject) {
                         return [
                             false,
@@ -40,6 +40,7 @@ module.exports = {
                 "explained-emoji-enum": (parsed, _when, emojisObject) => {
                     const { emoji } = parsed;
                     if (emoji && !Object.keys(emojisObject).includes(emoji)) {
+                        console.log(emoji,Object.keys(emojisObject))
                         return [
                             false,
                             `emoji must be one of:
@@ -61,12 +62,12 @@ module.exports = {
             {
                 "⭐️": "New feature",
                 "🛠️": "Bug fix",
-                "✅": "Add, update tests",
                 "🔥": "Hotfix",
                 "♻️": "Refactor",
                 "📝": "Documentation update",
+                "❗": "Breaking changes",
             },
         ],
         "jira-ticket": [2, "always"]
     },
-};
+    };
