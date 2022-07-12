018f35da6a1c9b176a1736a2f7ee918af32a1cec
Gromov Kirill <k.gromov@corp.mail.ru>
2022-07-12 10:20:13 +0300

    feat [FPT-333] new test

--- release.config.js
+++ release.config.js
@@ -5,8 +5,8 @@ module.exports = {
   plugins: [
     ['@semantic-release/commit-analyzer',{
       releaseRules: [
-        {"type": "Fix", "scope":"README", "release": "patch"},
-        {"type": "Feat", "release": "minor"},
+        {"type": "fix", "scope":"README", "release": "patch"},
+        {"type": "feat", "release": "minor"},
       ],
       parserOpts: {
         headerPattern: new RegExp(/(^[\w]+) (?:\[(.*)\]\s)([^\[].+)/),
@@ -37,7 +37,7 @@ module.exports = {
         },
       }
     ],
-    // "@semantic-release/npm",
+    "@semantic-release/npm",
     [
       '@semantic-release/git',
       {
