f02d5b9df81c9c0e2242ca7c87dd11eb3a338574
Gromov Kirill <k.gromov@corp.mail.ru>
2022-07-14 15:18:16 +0300

    feat [FPT-444] updated  telegram msg

--- release.config.js
+++ release.config.js
@@ -40,7 +40,7 @@ module.exports = {
       "chats": [-1001749648308],
       "templates": {
         "fail"    : "An error occured while trying to publish the new version of <b>{name}</b>.\n<pre><code class='language-javascript'>{error}</code></pre>",
-        "success" : "A new version of <a href='{repository_url}'>{name}</a> has been released. Current version is <b>{version}</b>"
+        "success" : "Опубликована новая версия  <a href='{repository_url}'>{name}</a><b>{version}</b> Было добавлено {release_notes}"
       }
     }],
     [
