1dd89e77031efe4e7cb591310a02d57801935be7
Kirill Gromov <kirill@gromov.vk>
2022-07-25 16:12:34 +0300

    style [FPT-000] dfgdfgdg

--- .release/format-commit-msg.sh
+++ .release/format-commit-msg.sh
@@ -1,19 +1,19 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 echo 'TEST'
 arr=($(cat $1))
 type_commit=${arr[0]}
 jira_ticket=${arr[1]}
 message_commit=${arr[2]}
-declare -A EMOJIS
-EMOJIS=(
-[feat]="✨"
-[fix]="🐛"
-[refactor]="📦"
-[docs]="📚"
-[style]="💎"
-[refactor]="📦"
-[build]="🛠"
-[chore]="♻️"
-[revert]="🗑"
-)
-echo " emojis ${EMOJIS[*]}"
+types_commit=("feat" "fix" "docs" "style" "refactor" "build" "chore" "revert")
+emojis=("✨" "🐛" "📚" "💎" "📦" "🛠" "♻" "🗑")
+
+function get_index_emoji {
+for i in "${!types_commit[@]}";
+do
+    if [[ "${types_commit[$i]}" = "${type_commit}" ]];
+    then
+        return $(( $i ))
+    fi
+done
+}
+echo get_index_emoji
