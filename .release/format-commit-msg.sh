7857af234d6b662162a28ed23baf25b2ad831578
Kirill Gromov <kirill@gromov.vk>
2022-07-25 16:14:15 +0300

    style [FPT-000] dfgdfgdg

--- .release/format-commit-msg.sh
+++ .release/format-commit-msg.sh
@@ -4,13 +4,14 @@ arr=($(cat $1))
 type_commit=${arr[0]}
 jira_ticket=${arr[1]}
 message_commit=${arr[2]}
-types_commit=("feat" "fix" "docs" "style" "refactor" "build" "chore" "revert")
+types=("feat" "fix" "docs" "style" "refactor" "build" "chore" "revert")
 emojis=("✨" "🐛" "📚" "💎" "📦" "🛠" "♻" "🗑")
 
 function get_index_emoji {
-for i in "${!types_commit[@]}";
+  echo "${!types[@]}"
+for i in "${!types[@]}";
 do
-    if [[ "${types_commit[$i]}" = "${type_commit}" ]];
+    if [[ "${types[$i]}" = "${types}" ]];
     then
         return $(( $i ))
     fi
