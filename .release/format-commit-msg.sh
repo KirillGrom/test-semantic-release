22637682bbcb2413f1400f1e7160eadd8399851d
Kirill Gromov <kirill@gromov.vk>
2022-07-25 15:46:43 +0300

    feat [FPT-000] dfgdfgdg

--- .release/format-commit-msg.sh
+++ .release/format-commit-msg.sh
@@ -4,17 +4,16 @@ arr=($(cat $1))
 type_commit=${arr[0]}
 jira_ticket=${arr[1]}
 message_commit=${arr[2]}
-#declare -A EMOJIS
-#EMOJIS=(
-#[feat]="✨"
-#[fix]="🐛"
-#[refactor]="📦"
-#[docs]="📚"
-#[style]="💎"
-#[refactor]="📦"
-#[build]="🛠"
-#[chore]="♻️"
-#[revert]="🗑"
-#)
-#echo " emojis ${EMOJIS[build]}"
-declare -a array_name
+declare -a EMOJIS
+EMOJIS=(
+[feat]="✨"
+[fix]="🐛"
+[refactor]="📦"
+[docs]="📚"
+[style]="💎"
+[refactor]="📦"
+[build]="🛠"
+[chore]="♻️"
+[revert]="🗑"
+)
+echo " emojis ${EMOJIS[build]}"
