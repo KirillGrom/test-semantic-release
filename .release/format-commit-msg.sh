53f4106cce9094a3c0915cbb6772fc19d1b2204e
Kirill Gromov <kirill@gromov.vk>
2022-07-25 13:51:24 +0300

    feat [FPT-000] dfgdfgdg

--- .release/format-commit-msg.sh
+++ .release/format-commit-msg.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo 'TEST'
 message="$(cat $1)"
-pat='/(^[\w]+) (?:\[(.*)\]\s)([^\[].+)/'
+pat='(^[\w]+) (?:\[(.*)\]\s)([^\[].+)'
 [[ $message =~ $pat ]]
+echo $message
 echo "${BASH_REMATCH[0]}"
 echo "${BASH_REMATCH[1]}"
