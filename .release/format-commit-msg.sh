f803b0035262fdfe3fd4cb4820169a084b8d47d6
Kirill Gromov <kirill@gromov.vk>
2022-07-25 14:22:21 +0300

    feat [FPT-000] dfgdfgdg

--- .release/format-commit-msg.sh
+++ .release/format-commit-msg.sh
@@ -4,7 +4,5 @@ message="$(cat $1)"
 pat='/[^\s]*/'
 [[ $message =~ $pat ]]
 echo $message
-echo ${BASH_REMATCH[0]}
-echo ${BASH_REMATCH[1]}
-echo 'first url, second url, third url' | sed 's/.*second//'
+echo ${BASH_REMATCH}
 
