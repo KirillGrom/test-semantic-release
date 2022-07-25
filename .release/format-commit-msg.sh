5ab69d7f1606089d6b828d2da1eb7c5d8a711735
Kirill Gromov <kirill@gromov.vk>
2022-07-25 15:33:15 +0300

    feat [FPT-000] dfgdfgdg

--- .release/format-commit-msg.sh
+++ .release/format-commit-msg.sh
@@ -4,7 +4,15 @@ arr=($(cat $1))
 type_commit=${arr[0]}
 jira_ticket=${arr[1]}
 message_commit=${arr[2]}
-echo $type_commit
-echo $jira_ticket
-echo $message_commit
-
+declare -A emojis=(
+[feat]="✨"
+[fix]="🐛"
+[refactor]="📦"
+[docs]="📚"
+[style]="💎"
+[refactor]="📦"
+[build]="🛠"
+[chore]="♻️"
+[revert]="🗑"
+)
+echo ${emojis[style]}
